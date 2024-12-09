ba66151a2794906389f6afa78aacc095b104df14
Hasan Uyduran <hasan@pop-os.home>
2024-12-09 23:05:22 +0300

    api bağlantıları

--- src/Hava.js
+++ src/Hava.js
@@ -6,6 +6,13 @@ const Hava = () => {
     const [data, setData] = useState([]);
     const [cityName, setCityName] = useState('ankara');
     const [desc , setDesc] = useState('');
+    const [temp , setTemp] = useState('');
+    const [icon , setIcon] = useState('');
+    const [humidity , setHumidity] = useState('');
+    const [min , setMin] = useState('');
+    const [max , setMax] = useState('');
+    const [date, setDate] = useState(new Date().toLocaleDateString());
+    const [time, setTime] = useState(new Date().toLocaleTimeString().slice(0,5));
     function getWeather(cityName) {
         const url = `https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=${cityName}`;
         axios.get(url, {
@@ -17,48 +24,58 @@ const Hava = () => {
         .then(response => {
           console.log(response.data); // API'den dönen veriler
           setCityName(response.data.city);
-          setDesc(response.data.result[0].description);
+          setDesc(response.data.result[0].description.slice(0,1).toUpperCase()+response.data.result[0].description.slice(1));
+          setTemp(Math.floor(response.data.result[0].degree).toString() + "°C");
+          setIcon(response.data.result[0].icon);
+          setHumidity("%"+response.data.result[0].humidity);
+          setMin(Math.floor(response.data.result[0].min).toString() + "°C");
+          setMax(Math.floor(response.data.result[0].max).toString() + "°C");
+          setDate(response.data.result[0].date);
         })
         .catch(error => {
           console.error('Hata:', error);
         });
     }
+
     useEffect(() => {
         getWeather(cityName);
     }, []);
     console.log("şehir adi",cityName);
 
-    //<label className="text-3xl">İl Adı:</label>
-    //<input value={cityName} className="bg-blue-400  text-3xl" type="text"  onChange={(e) => setCityName(e.target.value)} />
-    //<button className="text-3xl bg-blue-500" onClick={() => getWeather(cityName)}>Getir</button>
-    //<h1 className="text-3xl">{desc}</h1>
+
     return <div>
+      <div>
+        <label className="text-3xl">İl Adı:</label>
+        <input value={cityName} className="bg-blue-400  text-3xl" type="text"  onChange={(e) => setCityName(e.target.value)} />
+        <button className="text-3xl bg-blue-500" onClick={() => getWeather(cityName)}>Getir</button>
+        <h1 className="text-3xl">{desc}</h1>
+      </div>
       <div className='flex flex-col w-full h-screen justify-center items-center text-white ]' 
       style={{
         backgroundImage: 'url("https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
       }}>
         <div className='flex justify-between items-center w-1/2 p-5 bg-[rgba(0,0,0,0.28)] rounded-t-2xl'> 
           <div className='flex justify-start w-1/3'>
-            <h1 className="text-8xl ">28°C</h1>
+            <h1 className="text-8xl ">{temp}</h1>
           </div>
           <div className='flex justify-center w-1/3'>
-            <img src="https://cdnydm.com/permedia/fji5nQfowvEYJBdnLsvrgA.png?size=512x512" alt="weather" className='w-2/3' />
+            <img src={icon} alt="weather" className='w-2/3' />
           </div>
           <div className='flex justify-end w-1/3'>
-            <h1 className="text-3xl ">Az Güneşli</h1>
+            <h1 className="text-3xl ">{desc}</h1>
           </div>
           
         </div>
         <div className='flex flex-col items-center justify-center p-5 bg-[rgba(0,0,0,0.28)] w-1/2 border-white-800 border-t rounded-b-2xl'>
           <div className='flex justify-between w-full text-2xl'>
-            <p>Hissedilen: 27°C</p>
-            <p>Nem: %35</p>
-            <p>Min: 10°C</p>
-            <p>Maks: 30°C</p>
+            <p>{temp}</p>
+            <p>Nem: {humidity}</p>
+            <p>Min: {min}</p>
+            <p>Maks: {max}</p>
           </div>
           <div >
-            <h1 className="text-9xl bg text-center">00:00</h1>
-            <h1 className="text-3xl bg text-center">01/01/2025</h1>
+            <h1 className="text-9xl bg text-center">{time}</h1>
+            <h1 className="text-3xl bg text-center">{date}</h1>
           </div>
         </div>
         <div className='flex justify-between px-5 py-2 bg-[rgba(0,0,0,0.28)] w-1/2 mt-5 rounded-2xl'>
