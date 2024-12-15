b2e87e264f5768896945522ebbb7afdfd7168cb1
hasanuyduran <hasanuyduran8080@gmail.com>
2024-12-15 18:04:00 +0300

    button style and responsible desing

--- src/Hava.js
+++ src/Hava.js
@@ -2,6 +2,8 @@ import React from 'react'
 import axios from 'axios';
 import { useState, useEffect } from 'react';
 import Days_weather from './Days_weather';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
 
 const Hava = () => {
     const [data, setData] = useState([]);
@@ -48,28 +50,32 @@ const Hava = () => {
     
 
 
-    return <div className='h-screen' style={{
+    return <div className='h-screen bg-cover p-3' style={{
       backgroundImage: 'url("https://images.unsplash.com/photo-1419833173245-f59e1b93f9ee?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")',
     }}>
-      <div className='flex justify-end'>
-        <label className="text-3xl mt-6 text-white bg-[rgba(0,0,0,0.28)]">İl Adı:</label>
-        <input value={cityName} className="mt-6 bg-[rgba(0,0,0,0.28)] text-white text-3xl" type="text"  onChange={(e) => setCityName(e.target.value)} />
-        <button className="text-3xl text-white mt-6 bg-[rgb(59,158,56)]" onClick={() => getWeather(cityName)}>Getir</button>
+        <div className='flex justify-end items-center pt-6 sm:pe-5 '>
+        <div className='bg-[rgba(0,0,0,0.41)] flex justify-end items-center rounded-r-full rounded-l-full lg:w-1/4 md:w-1/3 sm:w-1/2'>
+        <div className='h-full sm:w-full '>
+          <input value={cityName} className=" bg-[rgba(0,0,0,0)]  text-white text-4xl p-2 ps-6 focus:outline-none focus:ring-0 w-full"  type="text"  onChange={(e) => setCityName(e.target.value)} />
+        </div>
+        <button className="text-white bg-[rgba(0,0,0,0)] rounded-full border-2 border-white" onClick={() => getWeather(cityName)}><FontAwesomeIcon className='m-5 text-3xl' icon={faMagnifyingGlass} /></button>
+        </div>
+        
       </div>
       <div className='flex flex-col w-full mt-20 justify-center items-center text-white ]'>
-        <div className='flex justify-between items-center w-1/2 p-5 bg-[rgba(0,0,0,0.28)] rounded-t-2xl'> 
+        <div className='flex justify-between items-center lg:w-1/2 md:w-2/3 sm:w-5/6 w-full p-5 bg-[rgba(0,0,0,0.41)] rounded-t-2xl'> 
           <div className='flex justify-start w-1/3'>
-            <h1 className="text-8xl ">{temp}</h1>
+            <h1 className="text-5xl sm:text-8xl ">{temp}</h1>
           </div>
           <div className='flex justify-center w-1/3'>
             <img src={icon} alt="weather" className='w-2/3' />
           </div>
           <div className='flex justify-end w-1/3'>
-            <h1 className="text-3xl ">{desc}</h1>
+            <h1 className="text-2xl sm:text-3xl">{desc}</h1>
           </div>
           
         </div>
-        <div className='flex flex-col items-center justify-center p-5 bg-[rgba(0,0,0,0.28)] w-1/2 border-white-800 border-t rounded-b-2xl'>
+        <div className='flex flex-col items-center justify-center p-5 bg-[rgba(0,0,0,0.41)] lg:w-1/2 md:w-2/3 sm:w-5/6 w-full border-white-800 border-t rounded-b-2xl'>
           <div className='flex justify-between w-full text-2xl'>
             <p>{temp}</p>
             <p>Nem: {humidity}</p>
@@ -81,7 +87,7 @@ const Hava = () => {
             <h1 className="text-3xl bg text-center">{date}</h1>
           </div>
         </div>
-        <div className='flex justify-between px-5 py-2 bg-[rgba(0,0,0,0.28)] w-1/2 mt-5 rounded-2xl'>
+        <div className='flex justify-between px-5 py-2 bg-[rgba(0,0,0,0.41)] lg:w-1/2 md:w-2/3 sm:w-5/6 mt-5 rounded-2xl'>
           <div className='flex justify-center items-center'>
             {result.map((item) => (
               <Days_weather key={item.date} icon={item.icon} min={Math.floor(item.min).toString() + "°C"} max={Math.floor(item.max).toString() + "°C"} date={item.day} />
